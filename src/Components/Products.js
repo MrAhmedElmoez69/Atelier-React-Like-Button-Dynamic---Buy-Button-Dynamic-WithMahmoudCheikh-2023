10a148f4267f43057863682159a2907144fda237
MrAhmedElmoez69 <noomenahmed90@gmail.com>
2023-02-12 17:03:08 +0100

    update products

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,29 +1,59 @@
 import React, { Component } from "react";
 import { Container, Row } from "react-bootstrap";
 import Product from "./Product";
+import data from "../products.json";
+import { Alert } from "react-bootstrap";
 
 class Products extends Component {
-    array = [];
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.state = { data: data, visible: false };
+  }
 
-        var data = require("../products.json");
-        this.array = data.map((e) => (
+  buy(index) {
+    this.onShowAlert();
+    const nextData = this.state.data;
+    nextData[index]["quantity"] -= 1;
+    this.setState({ data: nextData });
+  }
+
+  onShowAlert = () => {
+    this.setState({ visible: true }, () => {
+      window.setTimeout(() => {
+        this.setState({ visible: false });
+      }, 2000);
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Alert variant="success">
+          <Alert.Heading>Hey, welcome to our shop MyStore</Alert.Heading>
+          Thank you for choosing our store , we hope you enjoy your shopping
+          experience!
+        </Alert>
+        <Row>
+          {data.map((elem, index) => (
             <Product
-                title={e.name}
-                description={e.description}
-                price={e.price}
-                image={e.img}
-                like={e.like}
-                quantity={e.quantity}
-                liked={false}
+              key={index}
+              title={elem.name}
+              description={elem.description}
+              price={elem.price}
+              image={elem.img}
+              like={elem.like}
+              quantity={elem.quantity}
+              buy={() => this.buy(index)}
+              disabled={this.state.data[index]["quantity"] <= 0 ? true : false}
             />
-        ));
-    }
-    render() {
-
-        return <Container><Row>{this.array}</Row></Container>;
-    }
+          ))}
+        </Row>
+        <Alert key="primary" show={this.state.visible}>
+          You bought an item
+        </Alert>
+      </Container>
+    );
+  }
 }
 
 export default Products;
